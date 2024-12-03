b4d0c83fc489d6a479e78c0ca54cd9c760dc3b8d
artpupser <artpupser@yandex.ru>
2024-12-03 16:54:32 +0300

    Create project

---
+++ Assets/Scripts/Bootstrap.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+internal sealed class Bootstrap : MonoBehaviour {
+	private void Awake() {
+
+	}
+	private void Start() {
+		Debug.Log("Application started!");
+	}
+}
